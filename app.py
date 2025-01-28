c0058f740e2a8c6f5167a60ff904a133c1d56791
Morgan-Elkins <me839@york.ac.uk>
2025-01-28 15:31:53 +0000

    Added templete index and dotenv

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+import os
+import dotenv
+from flask import Flask, render_template
+import boto3
 
-app = Flask(__name__)
+AWS_REGION = os.getenv("AWS_REGION")
+AWS_QUEUES = [os.getenv("AWS_Q1"), os.getenv("AWS_Q2"), os.getenv("AWS_Q3")]
 
-@app.route("/")
-def say_hello():
-    return "<h1>Hello test flask</h1>"
+def create_app():
+    app = Flask(__name__)
+    @app.route("/")
+    def home():
+        print(f"{AWS_REGION} {AWS_QUEUES[0]}")
+        return render_template('index.html')
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
